fb611e3d8d21af4b4fd00bbb0f52528a0fb264e3
강용석 <kangyongsuek@gmail.com>
2020-04-02 07:01:39 +0900

    init: reset cra project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from './pages/Main';
-// import * as serviceWorker from './serviceWorker';
-
-const serviceWorker = require('./serviceWorker')
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Main />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
